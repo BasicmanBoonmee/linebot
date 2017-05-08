d82ea2fa322fef3ba1e05a1b187b0fbeb2c7a924
Siriphan Boonmee <m.basicman@msn.com>
2017-05-08 16:47:20 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-    die('test');
 	require_once __DIR__ . '/vendor/autoload.php';
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('qHrTqVT/bSCXzWZxEoJKN3M/L3AFcc22bG/Nksp3GpJNJCqWJi8p8Z2XMmrgjoFEb97JF7R5kSAAprv7K4KGPyIRnsT/hJPdIvyBcOMHN6mw2fHr/45UCWB/HiR72oJst79wfFnoU9bg7nb+kSaGagdB04t89/1O/w1cDnyilFU=');
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'be68d4c33c85989848aca09ea9acb875']);
