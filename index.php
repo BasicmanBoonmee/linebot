cc74bff1df7dcfe5df1639b9224b252f0a0e044f
Siriphan Boonmee <m.basicman@msn.com>
2017-05-15 14:41:56 +0700

    update

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 
 
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('qHrTqVT/bSCXzWZxEoJKN3M/L3AFcc22bG/Nksp3GpJNJCqWJi8p8Z2XMmrgjoFEb97JF7R5kSAAprv7K4KGPyIRnsT/hJPdIvyBcOMHN6mw2fHr/45UCWB/HiR72oJst79wfFnoU9bg7nb+kSaGagdB04t89/1O/w1cDnyilFU=');
-	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'be68d4c33c85989848aca09ea9acb875']);
+
+    echo "httpClient : ".$httpClient;
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'be68d4c33c85989848aca09ea9acb875']);
 
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
     echo "TEST : ".print_r($textMessageBuilder->buildMessage(),true);
