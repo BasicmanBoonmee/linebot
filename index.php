9697240ba2d6fefe062ffccf8961994dab8878d5
Siriphan Boonmee <m.basicman@msn.com>
2017-05-15 16:18:47 +0700

    update

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 	require_once __DIR__ . '/vendor/autoload.php';
 
-
-
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('qHrTqVT/bSCXzWZxEoJKN3M/L3AFcc22bG/Nksp3GpJNJCqWJi8p8Z2XMmrgjoFEb97JF7R5kSAAprv7K4KGPyIRnsT/hJPdIvyBcOMHN6mw2fHr/45UCWB/HiR72oJst79wfFnoU9bg7nb+kSaGagdB04t89/1O/w1cDnyilFU=');
 
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'be68d4c33c85989848aca09ea9acb875']);
 
-    echo "TEST";
+    $bot->parseEventRequest("Test","X-Line-Signature");
 
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+    echo "TEST";
+    /*$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 
     //$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
     $response = $bot->pushMessage("@jpz2894z",$textMessageBuilder);
     if ($response->isSucceeded()) {
         echo 'Succeeded!';
         return;
-    }
+    }*/
     
     // Failed
-    echo $response->getHTTPStatus . ' ' . $response->getRawBody();
+    //echo $response->getHTTPStatus . ' ' . $response->getRawBody();
 ?>
